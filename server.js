f54a8b544909d5262f3caf2dda24f4890de341c6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-08-17 14:46:30 -0400

    simple response

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
-app.use(require('cookie-parser'));
-
+app.get('/', function (req, res) {
+  res.send('hi there');
+});
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
